2cae28d69a327d7f8334b7833cdb213ce63e8b7e
IzukiKa <isaacjroberts0505@gmail.com>
2023-01-19 21:43:45 +0000

    Create Apple Pie

--- src/main/java/net/izukika/buffet/items/RegisterItems.java
+++ src/main/java/net/izukika/buffet/items/RegisterItems.java
@@ -14,11 +14,14 @@ import static net.izukika.buffet.BuffetMod.MOD_ID;
 
 public class RegisterItems {
     public static final Item SAUSAGE_ROLL = new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(3).saturationModifier(2.0f).snack().meat().build()));
+    public static final Item APPLE_PIE = new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(8).saturationModifier(4.8f).build()));
 
     public static void register() {
         Registry.register(Registries.ITEM, new Identifier(MOD_ID,"sausage_roll"), SAUSAGE_ROLL);
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID,"apple_pie"), APPLE_PIE);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(content -> {
             content.addAfter(Items.COOKED_RABBIT, SAUSAGE_ROLL);
+            content.addAfter(Items.PUMPKIN_PIE, APPLE_PIE);
         });
     }
 }
